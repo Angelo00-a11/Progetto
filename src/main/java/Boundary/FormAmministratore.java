db861fec088dade4d9ed15ff6ce894331710dca2
MaybeUser04 <lucaesposito_04@libero.it>
2026-06-15 20:00:18 +0200

    prova push

--- src/main/java/Boundary/FormAmministratore.java
+++ src/main/java/Boundary/FormAmministratore.java
@@ -134,6 +134,8 @@ public class FormAmministratore {
         tblRistoranti.setModel(new DefaultTableModel(colonne, 0));
     }
 
+
+    
 /*
     public static void main(String[] args) {
         JFrame frame = new JFrame();
